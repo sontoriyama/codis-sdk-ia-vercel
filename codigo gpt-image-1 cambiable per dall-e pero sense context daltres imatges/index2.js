d45b3d664f75fc7d92c5465a520bcfe359f14c9f
sontoriyama <sontoriyama@gmail.com>
2025-07-10 02:48:39 +0200

    recuperant validesa primer script gpt-image-1

--- codigo gpt-image-1 cambiable per dall-e pero sense context daltres imatges/index2.js
+++ codigo gpt-image-1 cambiable per dall-e pero sense context daltres imatges/index2.js
@@ -5,16 +5,16 @@ import { writeFile } from 'fs/promises';
 
 async function main() {
   console.log('--- Inicio del script ---');
-  console.log('Generando imagen estilo Akira Toriyama...');
+  console.log('Generando imagen estilo Ghibli...');
 
   const image = await generateImage({
     model: openai.image('gpt-image-1'), // Otra opcion 'dall-e-3' pero no tiene contexto de otra imagen como el nuevo nativo de 4o. Aunque flux context es mejor en imagen con contexto de otra
-    prompt: 'Un robot y un monstruo luchando en una ciudad en una epica batalla estilo Akira Toriyama',
+    prompt: 'A cat flying over a city, in the style of Studio Ghibli animation',
     size: '1024x1024'
   });
 
   console.log('Imagen generada, guardando en disco...');
-  await writeFile('dbz-image.png', image.uint8Array);
+  await writeFile('ghibli-image.png', image.uint8Array);
 
   console.log('✅ Imagen guardada como ghibli-image.png');
   console.log('--- Fin del script ---');
